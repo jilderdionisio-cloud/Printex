76afe3fd539f3ed71b7a15a1ba634554ef47ebbc
Alexdionisio <jilder.dionisio@tecsup.edu.pe>
2025-11-14 22:38:25 -0500

    Proyecto Terminado

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
     use WithoutModelEvents;
 
     /**
-     * Seed the application's database.
+     * Inicia la base de datos de la aplicacion
      */
     public function run(): void
     {
